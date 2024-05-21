6e5a552a99de7acebcded9027ad6c92c3d4a6d93
Jordi <1019jordi@gmail.com.com>
2024-05-21 19:05:04 +0200

    update folder structure

--- controllers/test.php
+++ controllers/test.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include '../controllers/Database.php';
+include '../models/Database.php';
 $db = Database::getInstance();
 $prepare = $db->prepare("SELECT * FROM users");
 $prepare->execute();
